66d5dab259b1a355d63df83430fa9fa4da819209
bengris32 <bengris32@protonmail.ch>
2024-08-26 23:39:58 +0100

    rosemary: Patch ImsService.apk extract time
    Change-Id: I2ef962ddb5c738cc7682d2b5441929313ed1f321
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.py
+++ extract-files.py
@@ -41,6 +41,8 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libsink.so': blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so')
         .add_needed('libaudioclient_shim.so'),
+    'system/priv-app/ImsService/ImsService.apk': blob_fixup()
+        .apktool_patch('blob-patches/ImsService.patch', '-r'),
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
