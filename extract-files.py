a80910ebae32fe83db7fd0b2e6754d1fda0b665c
Giovanni Ricca <giovanniricca@proton.me>
2024-11-14 11:58:48 +0100

    rosemary: Migrate to py extract utils
    Change-Id: Id8aa8df49451f3ed293542c63de954fbe4806a92

---
+++ extract-files.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    'system_ext/lib64/libsink.so': blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so')
+        .add_needed('libshim_sink.so'),
+    'vendor/bin/mtk_agpsd': blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-v32.so')
+        .replace_needed('libssl.so', 'libssl-v32.so'),
+    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek', 'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
+    'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service': blob_fixup()
+        .replace_needed('android.hardware.power-V2-ndk_platform.so', 'android.hardware.power-V2-ndk.so'),
+    'vendor/lib64/libmtkcam_featurepolicy.so': blob_fixup()
+        .binary_regex_replace(b'\x34\xE8\x87\x40\xB9', b'\x34\x28\x02\x80\x52'),
+    ('vendor/lib64/libmtkcam_stdutils.so', 'vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so'): blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so', 'vendor/lib64/libcam.utils.sensorprovider.so', 'vendor/lib64/librgbwlightsensor.so'): blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
+    'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/lib64/libgf_hal.so': blob_fixup()
+        .binary_regex_replace(b'\xFF\xC3\x01\xD1\xFD\x7B\x06\xA9', b'\x00\x00\x80\xD2\xC0\x03\x5F\xD6'),
+    'vendor/lib64/hw/fingerprint.fpc.default.so': blob_fixup()
+        .binary_regex_replace(b'\xFF\x43\x01\xD1\xFD\x7B\x02\xA9', b'\xC0\x03\x5F\xD6\xFD\x7B\x02\xA9'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'rosemary',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    add_firmware_proprietary_file=True,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
