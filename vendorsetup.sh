b9cc72d268b14946a6139ad486c0df73659faa4a
omrxdev <omarmohammed3147@gmail.com>
2026-04-11 09:18:55 +0200

    revert to previous vendor.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,121 +1,59 @@
-#!/usr/bin/env bash
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
+export LC_ALL="C"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_BUILD_TYPE="Unofficial"
+export FOX_BUILD_DEVICE="rosemary"
+export FOX_MAINTAINER_PATCH_VERSION="1"
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export TW_FORCE_KEYMASTER_VER=true
+export FOX_VANILLA_BUILD=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+export OF_FORCE_PREBUILT_KERNEL=1
 
-FDEVICE="rosemary"
-#set -o xtrace
+# A/B
+export FOX_AB_DEVICE=1
 
-fox_get_target_device() {
-	local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-	if [ -n "$chkdev" ]; then
-		FOX_BUILD_DEVICE="$FDEVICE"
-	else
-		chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-	fi
-}
+# Virtual A/B (rosemary is VAB) - this auto-enables FOX_AB_DEVICE and FOX_VANILLA_BUILD
+export FOX_VIRTUAL_AB_DEVICE=1
 
-if [ -z "$1" ] && [ -z "$FOX_BUILD_DEVICE" ]; then
-	fox_get_target_device
-fi
+# Screen (rosemary is 20:9 = 2400/1080)
+export OF_SCREEN_H=2400
+export OF_STATUS_H=89
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_CLOCK_POS=1
 
-# Dirty Fix: Only declare orangefox vars when needed
-if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
-	echo -e "\x1b[96m[INFO]: Setting up OrangeFox build vars for rosemary...\x1b[m"
-	if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-		# Version / Maintainer infos
-		export OF_MAINTAINER="Woomymy"
-		export FOX_VERSION=R11.1_1
-		export FOX_BUILD_TYPE="Beta"
+# MTK decryption
+export OF_FORCE_USE_RECOVERY_FSTAB=1
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
 
-		# Device info
-		export OF_AB_DEVICE=1
-		export OF_VIRTUAL_AB_DEVICE=1
-		export TARGET_DEVICE_ALT="secret, maltose"
-		
-		# OTA / DM-Verity / Encryption
-		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-		export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-		
-		export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-		export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-		export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
-		export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+# Misc
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+export FOX_DELETE_AROMAFM=1
 
-		# Display / Leds
-		export OF_SCREEN_H="2400"
-		export OF_STATUS_H="100"
-		export OF_STATUS_INDENT_LEFT=48
-		export OF_STATUS_INDENT_RIGHT=48
-		export OF_HIDE_NOTCH=1
-		export OF_CLOCK_POS=1 # left and right clock positions available
-		export OF_USE_GREEN_LED=0
-		export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.mt6785.rc for more information
+# Prebuilt kernel - prevents 'NO KERNEL CONFIG' error
+export OF_FORCE_PREBUILT_KERNEL=1
 
-		# Other OrangeFox configs
-		export OF_ENABLE_LPTOOLS=1
-		export OF_ALLOW_DISABLE_NAVBAR=0
-        export OF_QUICK_BACKUP_LIST="/boot;/data;"
-		export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800" # Tue Jan 1 2019 00:00:00 GMT
-		export FOX_DELETE_AROMAFM=1
+# Xiaomi-specific
+export FOX_TARGET_DEVICES="rosemary,secret,maltose,rosemary_p"
+export OF_NO_REFLASH_CURRENT_ORANGEFOX=1
 
-        export BUNDLED_MAGISK_VER="25.2"
-        export BUNDLED_MAGISK_SUM="0bdc32918b6ea502dca769b1c7089200da51ea1def170824c2812925b426d509" # Sha256 sum of the prebuilt magisk
+# Screen Resolution
+export OF_SCREEN_H=2400
+export OF_STATUS_H=89
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_CLOCK_POS=1
 
-            if [ -f "${FOX_USE_SPECIFIC_MAGISK_ZIP}" -a "$(sha256sum "${FOX_USE_SPECIFIC_MAGISK_ZIP}" 2>/dev/null | awk '{print $1}')" != "${BUNDLED_MAGISK_SUM}" ]
-            then
-                echo -e "\e[96m[INFO]: Removing invalid magisk zip\e[m"
-                rm -v "${FOX_USE_SPECIFIC_MAGISK_ZIP}"
-            fi
+export OF_MANUAL_ROOT_VENDOR_ERROR_FIX=1
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+export OF_HIDE_NOTCH=1
 
-        if [[ ! -f "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]]
-        then
-            # Download prebuilt magisk for OrangeFox builds
-            echo -e "\e[96m[INFO]: Downloading Magisk v${BUNDLED_MAGISK_VER}\e[m"
-            
-            if [[ "$(command -v "curl")" ]]
-            then
-                if [[ ! -d "$(dirname "${FOX_USE_SPECIFIC_MAGISK_ZIP}")" ]]
-                then
-                    mkdir -p "$(dirname "${FOX_USE_SPECIFIC_MAGISK_ZIP}")"
-                fi
-
-                # Download magisk and verify it
-                curl -L --progress-bar "https://github.com/topjohnwu/Magisk/releases/download/v${BUNDLED_MAGISK_VER}/Magisk-v${BUNDLED_MAGISK_VER}.apk" -o "${FOX_USE_SPECIFIC_MAGISK_ZIP}"
-                DOWNLOADED_SUM="$(sha256sum "${FOX_USE_SPECIFIC_MAGISK_ZIP}" | awk '{print $1}')"
-                
-                if [[ "${DOWNLOADED_SUM}" != "${BUNDLED_MAGISK_SUM}" ]]
-                then
-                    echo -e "\e[91m[ERROR]: Donwloaded Magisk ZIP seems *corrupted*, removing it to protect user's safety\e[m"
-                    rm "${FOX_USE_SPECIFIC_MAGISK_ZIP}"
-                    unset "FOX_USE_SPECIFIC_MAGISK_ZIP"
-                else
-                    echo -e "\e[96m[INFO]: Downloaded Magisk v${BUNDLED_MAGISK_VER}\e[m"
-                fi
-            else
-                # Curl is supposed to be installed according to "Establishing a build environnement" section in AOSP docs
-                # If it isn't, warn the builder about it and fallback to default Magisk ZIP
-                echo -e "\e[91m[ERROR]: Curl not found!\e[m"
-                unset "FOX_USE_SPECIFIC_MAGISK_ZIP"
-            fi
-        fi
-    fi
-fi
+# Ccache
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_DIR=/tmp/ccache
+ccache -M 10G
